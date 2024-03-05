1b5aa6a6e528ba0ec2f1e9def24754f51070d6f9
William Rose <william@theroses.me.uk>
2024-03-05 03:29:24 +0000

    Change to NonZeroU64 to be more representative of Entity (post 0.13)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use glam::UVec3;
 use murmuration::octree::Octree;
+use std::num::NonZeroU64;
 
 fn main() {
     let mut tree = Octree::new();
     tree.add(
         UVec3::new(0b11111111111111111111111111111111, 0b001, 0b101),
-        1,
+        NonZeroU64::new(1).unwrap(),
     );
     tree.add(
         UVec3::new(0b01111111111111111111111111111111, 0b001, 0b111),
-        2,
+        NonZeroU64::new(2).unwrap(),
     );
     tree.add(
         UVec3::new(
@@ -17,11 +18,11 @@ fn main() {
             0b10000000000000000000000000000001,
             0b101,
         ),
-        3,
+        NonZeroU64::new(3).unwrap(),
     );
-    tree.add(UVec3::new(0b01, 0, 0), 4);
-    tree.add(UVec3::new(0b10, 0, 0), 5);
-    tree.add(UVec3::new(0b11, 0, 0), 6);
-    tree.add(UVec3::new(0b10, 0b10, 0), 7);
+    tree.add(UVec3::new(0b01, 0, 0), NonZeroU64::new(4).unwrap());
+    tree.add(UVec3::new(0b10, 0, 0), NonZeroU64::new(5).unwrap());
+    tree.add(UVec3::new(0b11, 0, 0), NonZeroU64::new(6).unwrap());
+    tree.add(UVec3::new(0b10, 0b10, 0), NonZeroU64::new(7).unwrap());
     println!("{:?}\n{:?}", tree, tree.get_single(UVec3::new(0b10, 0, 0)));
 }
