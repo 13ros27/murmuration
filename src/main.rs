b3b121e89f09f9bb3252a0c772954af6c211aee8
William Rose <william@theroses.me.uk>
2024-03-06 16:04:53 +0000

    Added some remove stuff in .main for testing

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,51 @@ fn main() {
     tree.add(UVec3::new(0b10, 0b10, 0), NonZeroU64::new(7).unwrap());
     tree.add(UVec3::new(0b10, 0b10, 0), NonZeroU64::new(77).unwrap());
     println!(
+        "Removal {}, {tree:?}",
+        tree.remove(
+            UVec3::new(0b11111111111111111111111111111111, 0b001, 0b101),
+            NonZeroU64::new(1).unwrap(),
+        )
+    );
+    println!(
+        "Removal {}, {tree:?}",
+        tree.remove(
+            UVec3::new(0b01111111111111111111111111111111, 0b001, 0b111),
+            NonZeroU64::new(2).unwrap(),
+        )
+    );
+    println!(
+        "Removal {}",
+        tree.remove(
+            UVec3::new(
+                0b01111111111111111111111111111111,
+                0b10000000000000000000000000000001,
+                0b101,
+            ),
+            NonZeroU64::new(3).unwrap(),
+        )
+    );
+    println!(
+        "Removal {}",
+        tree.remove(UVec3::new(0b01, 0, 0), NonZeroU64::new(4).unwrap(),)
+    );
+    println!(
+        "Removal {}",
+        tree.remove(UVec3::new(0b10, 0, 0), NonZeroU64::new(5).unwrap(),)
+    );
+    println!(
+        "Removal {}",
+        tree.remove(UVec3::new(0b11, 0, 0), NonZeroU64::new(6).unwrap(),)
+    );
+    println!(
+        "Removal {}",
+        tree.remove(UVec3::new(0b10, 0b10, 0), NonZeroU64::new(7).unwrap(),)
+    );
+    println!(
+        "Removal {}",
+        tree.remove(UVec3::new(0b10, 0b10, 0), NonZeroU64::new(77).unwrap(),)
+    );
+    println!(
         "{:?}\n{:?}",
         tree,
         tree.get(UVec3::new(0b10, 0b10, 0)).collect::<Vec<_>>()
