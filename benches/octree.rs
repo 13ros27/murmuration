cc75c70a55d2de09f35c420fb639cff5c35e7c2f
William Rose <william@theroses.me.uk>
2024-03-06 00:53:17 +0000

    Added some very unrepresentative spatialtree benchmarks as something to look at

--- benches/octree.rs
+++ benches/octree.rs
@@ -4,7 +4,13 @@ use murmuration::octree::Octree;
 use rand::distributions::{Distribution, Uniform};
 use std::num::NonZeroU64;
 
-criterion_group!(all, add_many, get_many);
+criterion_group!(
+    all,
+    add_many,
+    add_many_spatialtree,
+    get_many,
+    get_many_spatialtree
+);
 criterion_main!(all);
 
 fn add_many(c: &mut Criterion) {
@@ -43,15 +49,69 @@ fn get_many(c: &mut Criterion) {
         )
     }
 
-    c.bench_function("get_single 100_000", |b| {
+    c.bench_function("get_single", |b| {
         b.iter(|| {
-            for _ in 0..100_000 {
-                black_box(tree.get_single(UVec3::new(
-                    uniform.sample(&mut rng),
-                    uniform.sample(&mut rng),
-                    uniform.sample(&mut rng),
-                )));
+            black_box(tree.get_single(UVec3::new(
+                uniform.sample(&mut rng),
+                uniform.sample(&mut rng),
+                uniform.sample(&mut rng),
+            )));
+        })
+    });
+}
+
+fn add_many_spatialtree(c: &mut Criterion) {
+    let mut tree = spatialtree::OctTree::new();
+    let uniform = Uniform::new_inclusive(0, u32::MAX);
+    let mut rng = rand::thread_rng();
+
+    c.bench_function("spatial_tree add 100_000", |b| {
+        b.iter(|| {
+            for i in 0..100_000 {
+                tree.insert(
+                    spatialtree::CoordVec::new(
+                        [
+                            uniform.sample(&mut rng),
+                            uniform.sample(&mut rng),
+                            uniform.sample(&mut rng),
+                        ],
+                        32,
+                    ),
+                    |_| NonZeroU64::new(i + 1).unwrap(),
+                );
             }
+        })
+    });
+}
+
+fn get_many_spatialtree(c: &mut Criterion) {
+    let mut tree = spatialtree::OctTree::new();
+    let uniform = Uniform::new_inclusive(0, u32::MAX);
+    let mut rng = rand::thread_rng();
+    for i in 0..100_000 {
+        tree.insert(
+            spatialtree::CoordVec::new(
+                [
+                    uniform.sample(&mut rng),
+                    uniform.sample(&mut rng),
+                    uniform.sample(&mut rng),
+                ],
+                32,
+            ),
+            |_| NonZeroU64::new(i + 1).unwrap(),
+        );
+    }
+
+    c.bench_function("spatial_tree get", |b| {
+        b.iter(|| {
+            black_box(tree.get_chunk_by_position(spatialtree::CoordVec::new(
+                [
+                    uniform.sample(&mut rng),
+                    uniform.sample(&mut rng),
+                    uniform.sample(&mut rng),
+                ],
+                32,
+            )))
         })
     });
 }
