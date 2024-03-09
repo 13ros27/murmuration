94bbd1ae21ff46a9ed4e224fa5e501b9ac191bf5
William Rose <william@theroses.me.uk>
2024-03-09 01:07:50 +0000

    Add basic documentation to SpatialGrid

--- src/grid.rs
+++ src/grid.rs
@@ -24,14 +24,63 @@ impl<P: Point> SpatialGrid<P> {
         self.0.move_data(old_point, new_point, entity)
     }
 
+    /// Returns the entity at the given point or `None` if there is nothing there.
+    ///
+    /// If there might be multiple entities in the same location you may want to use
+    /// [`get`](Self::get) instead.
+    ///
+    /// # Example
+    /// ```
+    /// # use bevy_ecs::prelude::*;
+    /// # use bevy_transform::prelude::*;
+    /// # use glam::Vec3;
+    /// # use murmuration::SpatialGrid;
+    /// /// Prints one of the entities at (0, 0, 0)
+    /// fn on_centre_system(grid: Res<SpatialGrid<Transform>>) {
+    ///     println!("{:?}", grid.get_single(&Transform::from_translation(Vec3::ZERO)));
+    /// }
+    /// ```
     pub fn get_single(&self, point: &P) -> Option<Entity> {
         self.get(point).next()
     }
 
+    /// Returns all the entities at the given `point`.
+    ///
+    /// If there is some small variation around the point, you may want to use [`within`](Self::within) with
+    /// a small distance instead.
+    ///
+    /// # Example
+    /// ```
+    /// # use bevy_ecs::prelude::*;
+    /// # use bevy_transform::prelude::*;
+    /// # use glam::Vec3;
+    /// # use murmuration::SpatialGrid;
+    /// /// Prints all the entities at exactly (0, 0, 0)
+    /// fn on_centre_system(grid: Res<SpatialGrid<Transform>>) {
+    ///     for entity in grid.get(&Transform::from_translation(Vec3::ZERO)) {
+    ///         println!("{:?}", entity);
+    ///     }
+    /// }
+    /// ```
     pub fn get(&self, point: &P) -> impl Iterator<Item = Entity> + '_ {
         self.0.get(point).copied()
     }
 
+    /// Returns all the entities within a radius `distance` of the given `point`.
+    ///
+    /// # Example
+    /// ```
+    /// # use bevy_ecs::prelude::*;
+    /// # use bevy_transform::prelude::*;
+    /// # use glam::Vec3;
+    /// # use murmuration::SpatialGrid;
+    /// /// Prints all the entities within 10 of (0, 0, 0)
+    /// fn near_centre_system(grid: Res<SpatialGrid<Transform>>) {
+    ///     for entity in grid.within(&Transform::from_translation(Vec3::ZERO), 10.0) {
+    ///         println!("{:?}", entity);
+    ///     }
+    /// }
+    /// ```
     pub fn within(&self, point: &P, distance: P::Data) -> impl Iterator<Item = Entity> + '_ {
         self.0.within(point, distance).copied()
     }
